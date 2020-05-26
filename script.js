56be9bf24cd6e99bb4028602ae53233fd05a0953
Freeda Moore <freedamoore@outlook.com>
2020-05-27 09:46:20 +1000

    added function to generate random house weapon. Currently returns human and computer choices in console.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const weapons = document.querySelectorAll(".weapon");
 
+const generateHouseWeapon = () => {
+  const weaponChoice = ['paper', 'scissors', 'rock'];
+  const randomWeapon = Math.floor(Math.random() * 3);
+  return weaponChoice[randomWeapon];
+}
+
 weapons.forEach(weapon => weapon.addEventListener('click', (weapon) => {
-  const currentWeapon = weapon.target;
-  console.log(currentWeapon.dataset.weapon);
+  const currentWeapon = weapon.target.dataset.weapon;
+  const computerWeapon = generateHouseWeapon();
+  console.log("Player weapon: " + currentWeapon);
+  console.log("Computer weapon: " + computerWeapon);
 }));

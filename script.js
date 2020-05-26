d57ddec01488b6421735672e6fc42b9e9b1c9f88
Freeda Moore <freedamoore@outlook.com>
2020-05-27 09:33:03 +1000

    small amendment

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const weapons = document.querySelectorAll(".weapon");
 
-
-
 weapons.forEach(weapon => weapon.addEventListener('click', (weapon) => {
   const currentWeapon = weapon.target;
   console.log(currentWeapon.dataset.weapon);
